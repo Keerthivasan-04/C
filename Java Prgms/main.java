f26aed95c1f6baebb7454c941ee7ee1f5d520d5d
Keerthivasan <131132737+Keerthivasan-04@users.noreply.github.com>
2024-01-31 13:39:17 +0530

    Add files via upload

---
+++ Java Prgms/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        int c=1;
+        while(c!=5)
+        {
+            System.out.println(c);
+            c++;
+        }
+
+    }
+}
